bee8d0d3417e2e166e09531921b3b289cb59e2bc
Esperanza Freeman <el.freeman1@yahoo.com>
2017-11-09 20:05:02 -0500

    edit

--- javascript.js
+++ javascript.js
@@ -1,6 +1,28 @@
+
+// ### Instructions
+//
+// 1. Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
+//    * We chose animals for our theme, but you can make a list to your own liking.
+//
+// 2. Your app should take the topics in this array and create buttons in your HTML.
+//    * Try using a loop that appends a button for each string in the array.
+//
+// 3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+//
+// 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+//
+// 5. Under every gif, display its rating (PG, G, so on).
+//    * This data is provided by the GIPHY API.
+//    * Only once you get images displaying with button presses shoulsd you move on to the next step.
+//
+// 6. Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+//
+// 7. Deploy your assignment to Github Pages.
+//
+//
 $( document ).ready(function() {
 // An array of animals,new animals will be pushed into this array;
-var topics = ['cat', 'dog', 'owl', 'peacock', 'horse'];
+var topics = ['cat', 'dog', 'owl', 'peacock', 'horse', 'tiger', 'lion', 'cheetah', 'leopard', 'wolf'];
 // Creating Functions & Methods
 // Function that displays all gif buttons
 function displayGifButtons(){
@@ -8,14 +30,14 @@ function displayGifButtons(){
     for (var i = 0; i < topics.length; i++){
         var gifButton = $("<button>");
         gifButton.addClass("gif");
-        gifButton.addClass("btn")
+        gifButton.addClass("btn btn-primary")
         gifButton.attr("data-name", topics[i]);
         gifButton.text(topics[i]);
         $("#gifButtonsView").append(gifButton);
     }
 }
 // Function to add a new animal button
-function addNewButton(){
+function addAnimal(){
 
     $("#addGif").on("click", function(){
 
@@ -27,11 +49,11 @@ function addNewButton(){
 
     displayGifButtons();
     return false;
+  this.value = '';
     });
 }
-// Function to remove last action button
-    // Doesnt work properly yet removes all of the added buttons
-    // rather than just the last
+// Function to reset last action button
+
 function resetButton(){
     $("removeGif").on("click", function(){
 
@@ -78,16 +100,16 @@ function displayGifs(){
 }
 // Calling Functions & Methods
 displayGifButtons();
-addNewButton();
+addAnimal();
 resetButton();
 //Document Event Listeners
 $(document).on("click", ".gif", displayGifs);{
 
-$(".input").hide;
 }
 
-//
-//
+
+//user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
 $(document).on("click", ".image", function(){
     var state = $(this).attr('data-state');
     if ( state == 'still'){
